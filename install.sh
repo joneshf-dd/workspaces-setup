54ebfe38e8209bfbf877c9de91f403f93c197ec8
Hardy Jones <88002054+joneshf-dd@users.noreply.github.com>
2025-06-02 17:12:44 -0700

    Flip the logic
    We exit early if we've already installed `nix`.

--- install.sh
+++ install.sh
@@ -7,24 +7,25 @@ set -o pipefail
 IFS=$'\n\t'
 
 # Install `nix`
-if ! command -v nix &> /dev/null; then
-    echo "Nix is not installed. Installing..."
-
-    # We don't have enough permissions to run multi-user,
-    # so we use `--no-daemon`.
-    sh <(curl --location --proto '=https' --tlsv1.2 https://nixos.org/nix/install) --no-daemon --yes
-
-    # Add Nix to the PATH
-    source ~/.nix-profile/etc/profile.d/nix.fish
-
-    # Enable flakes
-    mkdir --parents ~/.config/nix
-    echo 'experimental-features = flakes nix-command' > ~/.config/nix/nix.conf
-
-    echo "Nix installed successfully."
-else
+if command -v nix &> /dev/null; then
     echo "Nix is already installed."
+    exit
 fi
+
+echo "Nix is not installed. Installing..."
+
+# We don't have enough permissions to run multi-user,
+# so we use `--no-daemon`.
+sh <(curl --location --proto '=https' --tlsv1.2 https://nixos.org/nix/install) --no-daemon --yes
+
+# Add Nix to the PATH
+source ~/.nix-profile/etc/profile.d/nix.fish
+
+# Enable flakes
+mkdir --parents ~/.config/nix
+echo 'experimental-features = flakes nix-command' > ~/.config/nix/nix.conf
+
+echo "Nix installed successfully."
 
 # Copy the script to initialize `home-manager` to the PATH.
 mkdir --parents ~/.local/bin
