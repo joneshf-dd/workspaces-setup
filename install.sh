6e36c89ecb9960d719893a4d01ee294b3510a2c6
Hardy Jones <88002054+joneshf-dd@users.noreply.github.com>
2025-06-02 20:07:37 -0700

    Inline the `initialize-home-manager.sh` file
    We're not locating the file properly. And rather than figure that out,
    we change the setup to inline the creation of the file. It really
    doesn't matter for this small script how we do this. So we do what seems
    easier (we'll probably regret this soon…).

--- install.sh
+++ install.sh
@@ -26,5 +26,21 @@ echo "Nix installed successfully."
 
 # Copy the script to initialize `home-manager` to the PATH.
 mkdir --parents ~/.local/bin
-cp initialize-home-manager.sh ~/.local/bin/initialize-home-manager
+cat <<EOF > ~/.local/bin/initialize-home-manager
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+IFS=$'\n\t'
+
+# Clone my \`home-manager\` repository
+git clone git@github.com:joneshf-dd/home-manager.git ~/.config/home-manager
+
+# Install \`home-manager\`
+# No clue why we need \`--no-write-lock-file\`,
+# but we can't \`nix run\` anything without it.
+nix run home-manager/release-25.05 --no-write-lock-file -- switch
+EOF
 chmod 0755 initialize-home-manager
