3d93702f33bceab58597b5b0d924187266e5080c
Hardy Jones <88002054+joneshf-dd@users.noreply.github.com>
2025-06-02 20:26:44 -0700

    Always run each step
    Instead of bailing early if the Nix setup is already done, we make it so
    the script continues to the next step anyway.
    
    This kind of forces us to make this a little more robust, and check for
    things before overwriting them.

--- install.sh
+++ install.sh
@@ -10,23 +10,27 @@ IFS=$'\n\t'
 if command -v nix &> /dev/null; then
     echo "Nix is already installed."
     exit
+else
+    echo "Nix is not installed. Installing..."
+
+    # We don't have enough permissions to run multi-user,
+    # so we use `--no-daemon`.
+    sh <(curl --location --proto '=https' --tlsv1.2 https://nixos.org/nix/install) --no-daemon --yes
+
+    # Enable flakes
+    mkdir --parents ~/.config/nix
+    echo 'experimental-features = flakes nix-command' > ~/.config/nix/nix.conf
+
+    echo "Nix installed successfully."
 fi
 
-echo "Nix is not installed. Installing..."
-
-# We don't have enough permissions to run multi-user,
-# so we use `--no-daemon`.
-sh <(curl --location --proto '=https' --tlsv1.2 https://nixos.org/nix/install) --no-daemon --yes
-
-# Enable flakes
-mkdir --parents ~/.config/nix
-echo 'experimental-features = flakes nix-command' > ~/.config/nix/nix.conf
-
-echo "Nix installed successfully."
-
-# Copy the script to initialize `home-manager` to the PATH.
-mkdir --parents ~/.local/bin
-cat <<EOF > ~/.local/bin/initialize-home-manager
+# Write the script to initialize `home-manager` to the PATH.
+if command -v initialize-home-manager &> /dev/null; then
+    echo "initialize-home-manager is already in PATH."
+    exit
+else
+    mkdir --parents ~/.local/bin
+    cat <<EOF > ~/.local/bin/initialize-home-manager
 #!/usr/bin/env bash
 
 set -o errexit
@@ -43,4 +47,5 @@ git clone git@github.com:joneshf-dd/home-manager.git ~/.config/home-manager
 # but we can't \`nix run\` anything without it.
 nix run home-manager/release-25.05 --no-write-lock-file -- switch
 EOF
-chmod 0755 ~/.local/bin/initialize-home-manager
+    chmod 0755 ~/.local/bin/initialize-home-manager
+fi
