a3d9d0c14f59d2553b7c3a3f7d5905ddd49e2c7f
Hardy Jones <88002054+joneshf-dd@users.noreply.github.com>
2025-06-02 17:10:34 -0700

    Move more logic into install branch
    Just a little re-organizing. The only time we really need to setup the
    `flake` stuff, is when we're installing. After that point, we shouldn't
    have to do it again.

--- install.sh
+++ install.sh
@@ -9,20 +9,22 @@ IFS=$'\n\t'
 # Install `nix`
 if ! command -v nix &> /dev/null; then
     echo "Nix is not installed. Installing..."
+
     # We don't have enough permissions to run multi-user,
     # so we use `--no-daemon`.
     sh <(curl --location --proto '=https' --tlsv1.2 https://nixos.org/nix/install) --no-daemon --yes
+
+    # Add Nix to the PATH
     source ~/.nix-profile/etc/profile.d/nix.fish
+
+    # Enable flakes
+    mkdir --parents ~/.config/nix
+    echo 'experimental-features = flakes nix-command' > ~/.config/nix/nix.conf
+
     echo "Nix installed successfully."
 else
     echo "Nix is already installed."
 fi
-
-# Enable flakes
-mkdir --parents ~/.config/nix
-cat <<EOF > ~/.config/nix/nix.conf
-experimental-features = nix-command flakes
-EOF
 
 # Copy the script to initialize `home-manager` to the PATH.
 mkdir --parents ~/.local/bin
